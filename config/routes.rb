cc56b057f0befd3c1bb4128dad9d1d3b410902ea
SSENABULYA <ssenabulyawilsonm@gmail.com>
2016-10-09 23:15:09 +0300

    added dashboard links

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,14 @@ Rails.application.routes.draw do
   # 	get 'podcasts/show'
 
   	devise_for :podcasts
-  	root 'welcome#index'
   	resources :podcasts, only: [:index, :show]  do
       resources :episodes
     end
-
+    #if you are authenticated, you are redirected to the dashboard
+    authenticated :podcast do 
+      root 'podcasts#dashboard', as: "authenticated_root"
+    end
+    root 'welcome#index' #if you are not signed in
   # get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
